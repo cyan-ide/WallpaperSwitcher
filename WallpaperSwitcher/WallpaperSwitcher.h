5d1ce9f260f46cb9c350ac0b5914dda7662733d0
adam <adam-westerski@i2r.a-star.edu.sg>
2019-03-05 18:09:27 +0800

    Initial Commit

---
+++ WallpaperSwitcher/WallpaperSwitcher.h
@@ -0,0 +1,15 @@
+//
+//  WallpaperSwitcher.h
+//  WallpaperSwitcher
+//
+//  Created by adam on 5/3/19.
+//  Copyright © 2019 Adam Westerski. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface WallpaperSwitcher : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ WallpaperSwitcher/WallpaperSwitcher.m
@@ -0,0 +1,17 @@
+//
+//  WallpaperSwitcher.m
+//  WallpaperSwitcher
+//
+//  Created by adam on 5/3/19.
+//  Copyright © 2019 Adam Westerski. All rights reserved.
+//
+
+#import "WallpaperSwitcher.h"
+
+@implementation WallpaperSwitcher
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
